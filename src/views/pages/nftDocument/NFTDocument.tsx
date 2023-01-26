66ca69190fb5c53335a507a8083a7aa6a3612150
AbyulStudy <kinsng1599@gmail.com>
2023-01-26 09:59:25 +0900

    :x:working: NFTDocument 모듈화 필요

--- src/views/pages/nftDocument/NFTDocument.tsx
+++ src/views/pages/nftDocument/NFTDocument.tsx
@@ -21,6 +21,10 @@ import BottomNav from '../../../components/bottomNav/BottomNav';
 import TopNav from '../../../components/topNav/TopNav';
 import {globalConfig} from '../../../@config/config';
 import API_Mypage from '../../../@api/mypage/Mypage';
+import {globalStyles} from '../../../assets/css/global/styleSheet';
+import {Weight} from '../../../assets/css/global/font';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {MarginBottom, MarginTop} from '../../../assets/css/global/margin';
 
 interface CapitalInfo {
   Capital: any;
@@ -33,13 +37,14 @@ const NFTDocument = ({navigation}: any) => {
   const {URL} = globalConfig;
   const [nftInfo, setNftInfo] = useState<ENftInfo>();
   const [nftImgUri, setNftImgUri] = useState<string>();
+  const [ratio, setRatio] = useState(1);
   const [metaInfo, setMetaInfo] = useState<any>();
   const [capital, setCapital] = useState<CapitalInfo>();
 
   const getCapitalInfo = async () => {
     try {
       const data = await NFT_SERVICE.GET();
-      console.log('NFT', data);
+      console.log('getCapitalInfo : ', data);
       setNftInfo(data);
     } catch (error) {
       const success = await TOKEN_SERVICE.REFRESH__TOKEN();
@@ -55,10 +60,9 @@ const NFTDocument = ({navigation}: any) => {
 
   const getCapital = async () => {
     try {
-      const result = await MYypage_SERVICE.getMyData();
-      console.log('capital', result);
-      setCapital(result);
-      console.log('tw', result);
+      const data = await MYypage_SERVICE.getMyData();
+      console.log('getCapital : ', data);
+      setCapital(data);
     } catch (error) {
       console.log(error);
     }
@@ -73,10 +77,18 @@ const NFTDocument = ({navigation}: any) => {
       const imgUrl = URL.IMG + nftInfo?.ImgName + `?type=${nftInfo?.Category}`;
       setNftImgUri(imgUrl);
 
-      const metaInfo = JSON.parse(nftInfo.MetaInfo);
-      setMetaInfo(metaInfo);
+      // const metaInfo = JSON.parse(nftInfo?.MetaInfo);
+      // setMetaInfo(metaInfo);
     }
   }, [nftInfo]);
+
+  useEffect(() => {
+    if (nftImgUri) {
+      Image.getSize(nftImgUri, (width, height) => {
+        setRatio(width / height);
+      });
+    }
+  }, [nftImgUri]);
 
   useEffect(() => {
     const backAction = () => {
@@ -93,31 +105,64 @@ const NFTDocument = ({navigation}: any) => {
   }, []);
 
   return (
-    <View>
+    <View style={globalStyles.BodyWrap}>
       <TopNav navigation={navigation} title="NFT보증서" />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
-        style={styles.scrollView}>
-        <View style={styles.container}>
-          <Text style={styles.titleCode}>{nftInfo?.VehicleId}</Text>
+        style={globalStyles.ScrollViewNft}>
+        <View style={globalStyles.MainWrap}>
+          <Text
+            style={{
+              fontSize: 22,
+              lineHeight: 35,
+              ...Weight.Bold,
+              ...Colors[292929],
+            }}>
+            {nftInfo?.VehicleId}
+          </Text>
 
-          <Image style={styles.documentImage} source={{uri: nftImgUri}} />
-          <View style={{display: 'flex', flexDirection: 'row'}}>
-            <Text style={styles.text}>Owned by</Text>
-            <Text style={styles.text2}>
+          <View style={MarginTop(15)} />
+          <Image
+            style={{
+              width: '100%',
+              height: undefined,
+              aspectRatio: ratio,
+              resizeMode: 'contain',
+              borderRadius: 15,
+            }}
+            source={{uri: nftImgUri}}
+          />
+          <View style={MarginBottom(15)} />
+          <View style={globalStyles.FlexRow}>
+            <Text style={{fontSize: 16, ...Colors[666666], ...Weight.Normal}}>
+              Owned by
+            </Text>
+            <Text> </Text>
+            <Text style={{fontSize: 16, ...Colors.SkyBlue, ...Weight.Normal}}>
               {capital?.Capital !== '' ? capital?.Capital : null}
             </Text>
           </View>
-          <View style={styles.descriptionBox}>
-            <View style={styles.descriptionTitleContainer}>
-              <Text style={styles.text}>Description</Text>
-              <Image
-                style={{marginLeft: '2%'}}
-                source={require('../../../assets/logos_ethereum.png')}
-              />
+          <View style={MarginTop(25)} />
+          <View
+            style={{
+              backgroundColor: 'white',
+              borderRadius: 15,
+              padding: 15,
+            }}>
+            <View
+              style={{
+                flexDirection: 'row',
+                marginBottom: 10,
+              }}>
+              <Text
+                style={{fontSize: 16, ...Colors[666666], ...Weight.Default}}>
+                Description
+              </Text>
+              <Text>　</Text>
+              <Image source={require('../../../assets/logos_ethereum.png')} />
             </View>
 
-            <Text style={{color: 'black'}}>
+            <Text style={{fontSize: 14, ...Colors[666666], ...Weight.Default}}>
               {/* {metaInfo?.description ||
                 'Example : 차량은 캐피탈사의 공식 보증된차이며, 한국보증협회의 엄격한 품질' +
                   '검사와 검사를 198회 통과한 인증 중고차임을 증명합니다.'} */}
@@ -125,91 +170,12 @@ const NFTDocument = ({navigation}: any) => {
               검사와 검사를 198회 통과한 인증 중고차임을 증명합니다.
             </Text>
           </View>
+          <View style={MarginTop(25)} />
         </View>
-        <View style={{marginBottom: 130}}></View>
       </ScrollView>
       <BottomNav navigation={navigation} />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    height: Dimensions.get('window').height - 80,
-  },
-  container: {
-    marginHorizontal: 30,
-    marginVertical: 15,
-  },
-  titleCode: {
-    fontSize: 22,
-    color: '#292929',
-    lineHeight: 35,
-    letterSpacing: -0.05,
-  },
-  documentImage: {
-    height: 500,
-    marginVertical: 10,
-    resizeMode: 'contain',
-  },
-  descriptionTitleContainer: {
-    flexDirection: 'row',
-    marginBottom: 10,
-  },
-  descriptionBox: {
-    marginTop: '15%',
-    height: 'auto',
-    backgroundColor: 'white',
-    borderRadius: 15,
-    padding: 15,
-  },
-  detailBox: {
-    marginTop: 20,
-    height: 'auto',
-    backgroundColor: 'white',
-    borderRadius: 15,
-    padding: 20,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  detailBoxLeft: {
-    flexDirection: 'row',
-  },
-  detailText: {
-    marginLeft: 10,
-    fontSize: 15,
-    letterSpacing: -0.05,
-    color: '#444444',
-  },
-  offerButton: {
-    marginTop: 20,
-    height: 51,
-    backgroundColor: '#DDE6F0',
-    borderRadius: 10,
-    padding: 15,
-    margint: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  offerButtonText: {
-    color: '#226EC8',
-    fontSize: 18,
-    lineHeight: 18,
-  },
-  text: {
-    color: 'black',
-    fontFamily: 'Noto Sans',
-    fontSize: 16,
-    fontWeight: '400',
-  },
-  text2: {
-    color: '#34ABD0',
-    fontFamily: 'Noto Sans',
-    fontSize: 16,
-    fontWeight: '400',
-    marginLeft: '2%',
-  },
-});
 
 export default NFTDocument;
